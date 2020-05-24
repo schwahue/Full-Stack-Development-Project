8c1d23e3626af82ffd37d7d481aaca9c9e14a485
Tristan_HF <tristan.chf@gmail.com>
2020-05-24 17:58:50 +0800

    Userpage update

--- routes/user_route.js
+++ routes/user_route.js
@@ -2,28 +2,45 @@ const express = require('express');
 const router = express.Router();
 const alertMessage = require('../helpers/messenger.js');
 
+//HF Test Code
 router.get('/login', (req, res) => {
 
     res.render('user/login', {title:"login", style:"login_form"}); 
-
 });
 
 router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+    if (username == "admin" && password =="123456"){
+        res.redirect('/admin');
+    }
+    else if(username="test" && password == "test"){
+        res.redirect('/user/id');
+    }
+    else{
+        alertMessage(res, 'danger', 'Invalid Username or Password', 'fas fa-exclamation-circle', false);
 
-    res.render('user/login'); 
+        res.render('user/login',{title:"login", style:"login_form"}); 
+    }
 
+    
 });
 
 router.get('/signup', (req, res) => {
 
-    res.render('user/sign_up',  {title:"Sign Up", style:"login_form"}); 
+    res.render('user/sign_up',  {title:"Sign Up", style:"signup_form"}); 
 
 });
 
 router.post('/signup', (req, res) => {
 
-    res.render('user/sign_up'); 
+    res.render('user/sign_up'), {title:"Sign Up", style:"signup_form"}; 
 
+});
+
+
+router.get('/delivery', (req, res)=> {
+
+    res.render('user/delivery', {title:"delivery", style:"users"}); 
 });
 
 
@@ -33,12 +50,11 @@ router.get('/logout', (req, res) => {
 	res.redirect('/');
 });
 
-// Logout User
 router.get('/id', (req, res) => {
 
-	res.render('user/account');
+	res.render('user/account', {style:"users"});
 });
-
+//HF Test code End
 
 
 
